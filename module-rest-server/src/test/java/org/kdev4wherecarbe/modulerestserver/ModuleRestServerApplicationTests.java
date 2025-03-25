1bae7317b783250117c2ccd7e917a6132646a057
hjclover <107539614+hyeonjinan096@users.noreply.github.com>
2025-03-25 14:46:10 +0900

    [#2] init: 프로젝트 초기 구조 설정 (#3)

---
+++ module-rest-server/src/test/java/org/kdev4wherecarbe/modulerestserver/ModuleRestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kdev4wherecarbe.modulerestserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleRestServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ module-token-server/src/test/java/org/kdev4wherecarbe/moduletokenserver/ModuleTokenServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kdev4wherecarbe.moduletokenserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleTokenServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
