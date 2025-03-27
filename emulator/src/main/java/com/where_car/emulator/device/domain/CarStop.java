c4c0ec7e0156dcbe0c0f153d5b1b1e56269a631b
Laterre <laterre39@naver.com>
2025-03-27 17:14:02 +0900

    [#22] feat: 연동규격서에 따른 도메인 작성

---
+++ emulator/src/main/java/com/where_car/emulator/device/domain/CarStop.java
@@ -0,0 +1,26 @@
+package com.where_car.emulator.device.domain;
+
+import com.where_car.emulator.device.domain.common.CarDevice;
+import com.where_car.emulator.device.domain.common.CarIdentity;
+
+/**
+ * <pre>
+ *   시동 OFF 전달 도메인
+ *   carIdentity: 클래스 주석 참조
+ *   carDevice: 클래스 주석 참조
+ *   onTime: 차량 on 시간 | ccyyMMddHHmm
+ *   offTime: 차량 off 시간 | ccyyMMddHHmm
+ *   carCycleInfo: 클래스 주석 참조
+ * </pre>
+ * @author Changil.kim
+ * @version 1.0
+ * @since 2025-03-27
+ */
+public class CarStop {
+
+  private CarIdentity carIdentity;
+  private CarDevice carDevice;
+
+  private String onTime;
+  private String offTime;
+}
---
+++ emulator/src/main/java/com/where_car/emulator/device/domain/GetSetInfo.java
@@ -0,0 +1,22 @@
+package com.where_car.emulator.device.domain;
+
+import com.where_car.emulator.device.domain.common.CarDevice;
+import com.where_car.emulator.device.domain.common.CarIdentity;
+import java.time.LocalDateTime;
+
+/**
+ * <pre>
+ *   제어정보 요청 | 후순위
+ * </pre>
+ * @author Changil.kim
+ * @version 1.0
+ * @since 2025-03-27
+ */
+public class GetSetInfo {
+
+  private CarIdentity carIdentity;
+  private CarDevice carDevice;
+
+  private LocalDateTime onTime;
+  private String dFWVer = "LTE 1.2";
+}
