d7151a2b7c59295de5eeaf0b7fa9556e435363c1
hyeonjinan096 <hyeonjinan096@naver.com>
2025-04-02 12:40:25 +0900

    [#61] modify: 위도 경도 타입 변경

--- rest/src/main/java/com/wherecar/rest/domain/GpsLog.java
+++ rest/src/main/java/com/wherecar/rest/domain/GpsLog.java
@@ -31,10 +31,10 @@ public class GpsLog extends BaseEntity{
     private String gpsCondition;
 
     @Column(name = "latitude")
-    private Integer latitude;
+    private Double latitude;
 
     @Column(name = "longitude")
-    private Integer longitude;
+    private Double longitude;
 
     @Column(name = "angle")
     private Integer angle;
