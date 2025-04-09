2b04e3cdd83e2bb64ede65819ed16269b51743b5
hyeonjinan096 <hyeonjinan096@naver.com>
2025-04-10 02:07:18 +0900

    [#122] feat: 차량 실시간 데이터 전송 스케줄링 구현

--- rest/src/main/java/com/wherecar/rest/RestApplication.java
+++ rest/src/main/java/com/wherecar/rest/RestApplication.java
@@ -3,9 +3,11 @@ package com.wherecar.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableScheduling
 public class RestApplication {
 
 	public static void main(String[] args) {
