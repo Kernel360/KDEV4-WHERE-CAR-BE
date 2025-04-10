d6ceba0a82c3169003ccdfe000b50ee2f5cc8127
ijjung365 <ijjung365@gmail.com>
2025-04-10 15:10:29 +0900

    [#126] fix:  안쓰는 import 삭제

--- rest/src/main/java/com/wherecar/rest/geo/dto/GeoInfoRequest.java
+++ rest/src/main/java/com/wherecar/rest/geo/dto/GeoInfoRequest.java
@@ -1,6 +1,5 @@
 package com.wherecar.rest.geo.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.LocalDateTime;
--- rest/src/main/java/com/wherecar/rest/geo/dto/GeoInfoResponse.java
+++ rest/src/main/java/com/wherecar/rest/geo/dto/GeoInfoResponse.java
@@ -1,6 +1,5 @@
 package com.wherecar.rest.geo.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
