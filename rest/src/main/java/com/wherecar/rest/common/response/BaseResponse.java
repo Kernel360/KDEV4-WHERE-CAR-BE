65d0426ee7b0a6bd727e3d3992123828fc86d77e
ijjung365 <ijjung365@gmail.com>
2025-04-17 15:17:03 +0900

    [#157] modify: 사용안할 HttpStatusCode 제거

--- rest/src/main/java/com/wherecar/rest/common/response/BaseResponse.java
+++ rest/src/main/java/com/wherecar/rest/common/response/BaseResponse.java
@@ -2,7 +2,9 @@ package com.wherecar.rest.common.response;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
 
+@Data
 public class BaseResponse<T> {
     private T data;
     private String message;
@@ -38,6 +40,11 @@ public class BaseResponse<T> {
         return new BaseResponse<>(data, HttpStatusCode.CREATED);
     }
 
+    // 202 Accepted
+    public static <T> BaseResponse<T> accepted(String message) {
+        return new BaseResponse<>(null, HttpStatusCode.ACCEPTED);
+    }
+
     // 204 No Content
     public static <T> BaseResponse<T> noContent() {
         return new BaseResponse<>(null, HttpStatusCode.NO_CONTENT);
@@ -53,41 +60,22 @@ public class BaseResponse<T> {
         return new BaseResponse<>(message, HttpStatusCode.UNAUTHORIZED);
     }
 
+    // 403 Forbidden
+    public static <T> BaseResponse<T> forbidden(String message) {
+        return new BaseResponse<>(message, HttpStatusCode.FORBIDDEN);
+    }
+
     // 404 Not Found
     public static <T> BaseResponse<T> notFound(String message) {
         return new BaseResponse<>(message, HttpStatusCode.NOT_FOUND);
     }
 
-    // 409 Conflict
-    public static <T> BaseResponse<T> conflict(String message) {
-        return new BaseResponse<>(message, HttpStatusCode.CONFLICT);
-    }
 
     // 500 Internal Server Error
     public static <T> BaseResponse<T> internalServerError(String message) {
         return new BaseResponse<>(message, HttpStatusCode.INTERNAL_SERVER_ERROR);
     }
 
-    // 202 Accepted
-    public static <T> BaseResponse<T> accepted(String message) {
-        return new BaseResponse<>(null, HttpStatusCode.ACCEPTED);
-    }
-
-    // 403 Forbidden
-    public static <T> BaseResponse<T> forbidden(String message) {
-        return new BaseResponse<>(message, HttpStatusCode.FORBIDDEN);
-    }
-
-    // 405 Method Not Allowed
-    public static <T> BaseResponse<T> methodNotAllowed(String message) {
-        return new BaseResponse<>(message, HttpStatusCode.METHOD_NOT_ALLOWED);
-    }
-
-
-    // 503 Service Unavailable
-    public static <T> BaseResponse<T> serviceUnavailable(String message) {
-        return new BaseResponse<>(message, HttpStatusCode.SERVICE_UNAVAILABLE);
-    }
 
     @Override
     public String toString() {
@@ -98,4 +86,5 @@ public class BaseResponse<T> {
             return "{\"data\":null,\"message\":\"서버 내부 오류입니다(JSON 변환).\",\"statusCode\":500}";  // 예외 발생 시 빈 객체 반환
         }
     }
+
 }
