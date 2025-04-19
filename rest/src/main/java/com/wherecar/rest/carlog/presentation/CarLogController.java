886fbd71628de21eb34c49f9580d59ec842632bb
hyeonjinan096 <hyeonjinan096@naver.com>
2025-04-19 16:32:22 +0900

    [#166] fix: AuthUtil -> HttpServletRequest에서 companyId 가줘오도록 수정

--- rest/src/main/java/com/wherecar/rest/carlog/presentation/CarLogController.java
+++ rest/src/main/java/com/wherecar/rest/carlog/presentation/CarLogController.java
@@ -5,7 +5,7 @@ import com.wherecar.rest.common.constants.PaginationConstants;
 import com.wherecar.rest.carlog.application.dto.CarLogFilterRequest;
 import com.wherecar.rest.carlog.application.dto.CarLogsUpdateRequest;
 import com.wherecar.rest.carlog.application.CarLogService;
-import com.wherecar.rest.security.auth.AuthUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -23,11 +23,12 @@ class CarLogController {
     //운행일지 목록 조회 (filter 추가)
     @PostMapping
     public ResponseEntity<Page<CarLogResponse>> carLogsGetWithFilter(
+            HttpServletRequest httpServletRequest,
             @RequestParam(value = "page", defaultValue = "" + PaginationConstants.DEFAULT_PAGE) int page,
             @RequestParam(value = "size", defaultValue = "" + PaginationConstants.DEFAULT_SIZE) int size,
             @RequestBody(required = false) CarLogFilterRequest filterRequest) {
 
-        Long companyId = AuthUtil.getCompanyId();
+        Long companyId = (Long)httpServletRequest.getAttribute("companyId");
 
         CarLogFilterRequest request = filterRequest != null ? filterRequest : new CarLogFilterRequest();
 
@@ -68,9 +69,9 @@ class CarLogController {
 
     // 대시보드 운행 통계 달별 km 및 운행건수를 counting
     @GetMapping("/statics")
-    public ResponseEntity<CarLogResponse> carLogsStaticsGetAll() {
+    public ResponseEntity<CarLogResponse> carLogsStaticsGetAll(HttpServletRequest request) {
 
-        Long companyId = AuthUtil.getCompanyId();
+        Long companyId = (Long)request.getAttribute("companyId");
         System.out.println("companyId = " + companyId);
         CarLogResponse carLogsStaticsResponse = carLogService.getAllCarLogsStatics(companyId);
 
