0d247edccfe68b30d00387646aec1e974b2d3ed0
codingjjh0515 <greenjjh0515@naver.com>
2025-04-21 03:06:57 +0900

    [#180] modify: Collector의 엔터티들에 자동 생성 시간 적용

--- collector/src/main/java/com/wherecar/collector/CollectorApplication.java
+++ collector/src/main/java/com/wherecar/collector/CollectorApplication.java
@@ -2,8 +2,10 @@ package com.wherecar.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableJpaAuditing
 @EnableTransactionManagement
 @SpringBootApplication
 public class CollectorApplication {
