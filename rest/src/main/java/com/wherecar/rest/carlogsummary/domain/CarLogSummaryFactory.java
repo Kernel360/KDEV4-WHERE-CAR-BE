d32b1ea19fcdedc41064023c2a0b51c97ce4b954
ijjung365 <ijjung365@gmail.com>
2025-04-21 02:47:05 +0900

    [#183] feat: CarLogSummary를 기준으로 통계 생성

---
+++ rest/src/main/java/com/wherecar/rest/carlogsummary/domain/CarLogSummaryFactory.java
@@ -0,0 +1,90 @@
+package com.wherecar.rest.carlogsummary.domain;
+
+import com.wherecar.rest.carlogsummary.application.dto.CarLogSummaryOverviewResponse;
+import com.wherecar.rest.gpslog.application.dto.GpsPoint;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+public class CarLogSummaryFactory {
+    public CarLogSummaryOverviewResponse toCarLogSummaryOverviewResponse(List<CarLogSummary> carLogSummaries) {
+        int totalDistance = 0;
+        int maxDistance = 0;
+        int averageDistance;
+
+        int maxSpeed = 0;
+        int averageSpeed;
+
+        int totalDriveTime = 0;
+        int maxDriveTime = 0;
+        int averageDriveTime;
+
+        int unclassifiedCount = 0;
+        int commuteCount = 0;
+        int businessCount = 0;
+        int personalCount = 0;
+
+        List<GpsPoint> onList = new ArrayList<>();
+        List<GpsPoint> offList = new ArrayList<>();
+
+        for(CarLogSummary carLogSummary : carLogSummaries) {
+            maxDistance = Math.max(maxDistance, carLogSummary.getDistance());
+            maxSpeed = Math.max(maxSpeed, carLogSummary.getDistance());
+            maxDriveTime = Math.max(maxDriveTime,(int) Duration.between(carLogSummary.getOffTime(),carLogSummary.getOnTime()).toSeconds());
+
+            totalDistance += carLogSummary.getDistance();
+            totalDriveTime += (int) Duration.between(carLogSummary.getOffTime(),carLogSummary.getOnTime()).toSeconds();
+
+            switch (carLogSummary.getDriveType()) {
+                case UNCLASSIFIED -> unclassifiedCount++;
+                case PERSONAL     -> personalCount++;
+                case COMMUTE      -> commuteCount++;
+                case BUSINESS     -> businessCount++;
+            }
+
+
+            onList.add(GpsPoint.builder()
+                            .latitude(carLogSummary.getOnLatitude())
+                            .longitude(carLogSummary.getOnLongitude())
+                            .timestamp(carLogSummary.getOnTime())
+                    .build()
+            );
+
+            offList.add(GpsPoint.builder()
+                    .latitude(carLogSummary.getOffLatitude())
+                    .longitude(carLogSummary.getOffLongitude())
+                    .timestamp(carLogSummary.getOffTime())
+                    .build()
+            );
+        }
+
+        averageDistance = totalDistance / carLogSummaries.size();
+        averageSpeed = (totalDistance * 3600) / totalDriveTime;
+        averageDriveTime = totalDriveTime / carLogSummaries.size();
+
+
+
+
+        return CarLogSummaryOverviewResponse.builder()
+                .totalDistance(totalDistance)
+                .maxDistance(maxDistance)
+                .averageDistance(averageDistance)
+                .maxSpeed(maxSpeed)
+                .averageSpeed(averageSpeed)
+                .totalDriveTime(totalDriveTime)
+                .maxDriveTime(maxDriveTime)
+                .averageDriveTime(averageDriveTime)
+                .unclassifiedCount(unclassifiedCount)
+                .commuteCount(commuteCount)
+                .businessCount(businessCount)
+                .personalCount(personalCount)
+                .onList(onList)
+                .offList(offList)
+                .build();
+    }
+}
