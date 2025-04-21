52cd9b3d5d6f1565f1610bb1bfc874e74c8dd541
hyeonjinan096 <hyeonjinan096@naver.com>
2025-04-21 17:18:42 +0900

    [#190] feat: 에뮬레이터 토큰 로직 폴더 구조 설계

---
+++ rest/src/main/java/com/wherecar/rest/emulauth/application/EmulAuthServiceImpl.java
@@ -0,0 +1,4 @@
+package com.wherecar.rest.emulauth.application;
+
+public class EmulAuthServiceImpl implements EmulAuthService{
+}
---
+++ rest/src/main/java/com/wherecar/rest/emulauth/infrastructure/EmulTokenStoreImpl.java
@@ -0,0 +1,4 @@
+package com.wherecar.rest.emulauth.infrastructure;
+
+public class EmulTokenStoreImpl {
+}
