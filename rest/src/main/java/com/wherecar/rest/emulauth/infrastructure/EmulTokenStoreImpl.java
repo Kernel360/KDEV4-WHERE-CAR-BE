f8141684c5c78388324c787a310fc0f1c6bab9cb
hyeonjinan096 <hyeonjinan096@naver.com>
2025-04-21 19:03:59 +0900

    [#190] feat: 에뮬레이터 토큰 발급 및 레디스 연결 구현

--- rest/src/main/java/com/wherecar/rest/emulauth/infrastructure/EmulTokenStoreImpl.java
+++ rest/src/main/java/com/wherecar/rest/emulauth/infrastructure/EmulTokenStoreImpl.java
@@ -1,4 +1,28 @@
 package com.wherecar.rest.emulauth.infrastructure;
 
-public class EmulTokenStoreImpl {
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.time.Duration;
+
+@Repository
+@RequiredArgsConstructor
+public class EmulTokenStoreImpl implements EmulTokenStore{
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Override
+    public void saveToken(String mdn, String token, long expireSeconds) {
+        String key = "token:" + mdn;
+        redisTemplate.opsForValue().set(key, token, Duration.ofSeconds(expireSeconds));
+    }
+
+    @Override
+    public String getToken(String mdn) {
+        String key = "token:" + mdn;
+        Object value = redisTemplate.opsForHash().get(key, "token");
+        return value != null ? value.toString() : null;
+    }
+
 }
--- rest/src/main/java/com/wherecar/rest/emulauth/presentation/EmulAuthController.java
+++ rest/src/main/java/com/wherecar/rest/emulauth/presentation/EmulAuthController.java
@@ -1,4 +1,28 @@
 package com.wherecar.rest.emulauth.presentation;
 
+
+import com.wherecar.rest.emulauth.application.EmulAuthService;
+import com.wherecar.rest.emulauth.application.dto.EmulTokenRequestDto;
+import com.wherecar.rest.emulauth.application.dto.EmulTokenResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/emulator")
+@RequiredArgsConstructor
 public class EmulAuthController {
+
+    private final EmulAuthService emulAuthService;
+
+    @PostMapping("/token")
+    public ResponseEntity<EmulTokenResponseDto> getToken(@RequestBody EmulTokenRequestDto requestDto) {
+        EmulTokenResponseDto responseDto = emulAuthService.issueToken(requestDto);
+        return ResponseEntity.ok(responseDto);
+    }
+
+
 }
