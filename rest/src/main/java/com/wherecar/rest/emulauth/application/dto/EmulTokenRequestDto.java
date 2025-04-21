97872b5c0174103690c7936a6de88443396fe137
hyeonjinan096 <hyeonjinan096@naver.com>
2025-04-21 17:33:27 +0900

    [#190] feat: 에뮬레이터 토큰 request, response DTO 구현

--- rest/src/main/java/com/wherecar/rest/emulauth/application/dto/EmulTokenRequestDto.java
+++ rest/src/main/java/com/wherecar/rest/emulauth/application/dto/EmulTokenRequestDto.java
@@ -1,4 +1,17 @@
 package com.wherecar.rest.emulauth.application.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class EmulTokenRequestDto {
+
+    private String mdn;
+    private String tid;
+    private String mid;
+    private String pv;
+    private String did;
+    private String dFWVer;
 }
+
--- rest/src/main/java/com/wherecar/rest/emulauth/application/dto/EmulTokenResponseDto.java
+++ rest/src/main/java/com/wherecar/rest/emulauth/application/dto/EmulTokenResponseDto.java
@@ -1,4 +1,15 @@
 package com.wherecar.rest.emulauth.application.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class EmulTokenResponseDto {
+
+    private final String rstCd;
+    private final String rstMsg;
+    private final String mdn;
+    private final String token;
+    private final String exPeriod;
 }
