4ceac8af167cdc35d3cdce881b555122493323ff
ijjung365 <ijjung365@gmail.com>
2025-04-23 21:29:21 +0900

    [#199] modify: LocalDateTime 이 일관적인 포맷으로 리턴되도록 변경

--- rest/src/main/java/com/wherecar/rest/gpslog/application/dto/GpsLogResponse.java
+++ rest/src/main/java/com/wherecar/rest/gpslog/application/dto/GpsLogResponse.java
@@ -1,5 +1,6 @@
 package com.wherecar.rest.gpslog.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,5 +12,7 @@ public class GpsLogResponse {
     private String mdn;
     private double latitude;
     private double longitude;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
