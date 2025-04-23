a14d8cde31dffda18be10afda997dfbfc37ade79
ijjung365 <ijjung365@gmail.com>
2025-04-23 14:48:13 +0900

    [#199] modify: AnnouncementController 수정 및 테스트

--- rest/src/main/java/com/wherecar/rest/announcement/presentation/AnnouncementController.java
+++ rest/src/main/java/com/wherecar/rest/announcement/presentation/AnnouncementController.java
@@ -1,18 +1,18 @@
 package com.wherecar.rest.announcement.presentation;
 
+import com.wherecar.rest.announcement.application.AnnouncementService;
 import com.wherecar.rest.announcement.application.dto.AnnouncementRequest;
 import com.wherecar.rest.announcement.application.dto.AnnouncementResponse;
-import com.wherecar.rest.announcement.application.AnnouncementService;
 import com.wherecar.rest.common.constants.PaginationConstants;
+import com.wherecar.rest.common.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/announcement")
+@RequestMapping("/api/announcements")
 @RequiredArgsConstructor
 public class AnnouncementController {
 
@@ -20,45 +20,45 @@ public class AnnouncementController {
 
     // 공지 사항 등록
     @PostMapping
-    public ResponseEntity<Void> announcementCreate(@RequestBody AnnouncementRequest announcementRequest) {
-        announcementService.createAnnouncement(announcementRequest);
+    public BaseResponse<AnnouncementResponse> announcementCreate(@RequestBody AnnouncementRequest announcementRequest) {
+        AnnouncementResponse announcementResponse = announcementService.createAnnouncement(announcementRequest);
 
-        return ResponseEntity.ok().build();
+        return BaseResponse.created(announcementResponse);
     }
 
     // 공지 사항 목록 조회
     @GetMapping
-    public ResponseEntity<Page<AnnouncementResponse>> announcementsGet(
+    public BaseResponse<Page<AnnouncementResponse>> announcementsGet(
             @RequestParam(value = "page", defaultValue = "" + PaginationConstants.DEFAULT_PAGE) int page,
             @RequestParam(value = "size", defaultValue = "" + PaginationConstants.DEFAULT_SIZE) int size)
     {
         Page<AnnouncementResponse> announcements = announcementService.getAnnouncements(page, size);
 
-        return ResponseEntity.ok(announcements);
+        return BaseResponse.ok(announcements);
     }
 
     // 공지 사항 글 조회
     @GetMapping("/{announcementId}")
-    public ResponseEntity<AnnouncementResponse> announcementGetDetail(@PathVariable Long announcementId) {
+    public BaseResponse<AnnouncementResponse> announcementGetDetail(@PathVariable Long announcementId) {
         AnnouncementResponse announcement = announcementService.getAnnouncementDetail(announcementId);
 
-        return ResponseEntity.ok(announcement);
+        return BaseResponse.ok(announcement);
     }
 
     // 공지 사항 수정
     @PutMapping("/{announcementId}")
-    public ResponseEntity<Void> announcementUpdate(@PathVariable Long announcementId, @RequestBody AnnouncementRequest announcementRequest) {
-        announcementService.updateAnnouncement(announcementId, announcementRequest);
+    public BaseResponse<AnnouncementResponse> announcementUpdate(@PathVariable Long announcementId, @RequestBody AnnouncementRequest announcementRequest) {
+        AnnouncementResponse announcementResponse = announcementService.updateAnnouncement(announcementId, announcementRequest);
 
-        return ResponseEntity.ok().build();
+        return BaseResponse.created(announcementResponse);
     }
 
     // 공지 사항 삭제
     @DeleteMapping("/{announcementId}")
-    public ResponseEntity<Void> announcementDelete(@PathVariable Long announcementId) {
+    public BaseResponse<Void> announcementDelete(@PathVariable Long announcementId) {
         announcementService.deleteAnnouncement(announcementId);
 
-        return ResponseEntity.ok().build();
+        return BaseResponse.ok();
     }
 
 }
