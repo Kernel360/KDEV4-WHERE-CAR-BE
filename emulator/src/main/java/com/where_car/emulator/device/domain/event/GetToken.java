211e7df7fe476288c47c39de767ebd850e409b23
Changil.Kim <laterre39@naver.com>
2025-04-25 10:15:18 +0900

    [#202] refactor: 도메인 클래스 리팩토링

--- emulator/src/main/java/com/where_car/emulator/device/domain/event/GetToken.java
+++ emulator/src/main/java/com/where_car/emulator/device/domain/event/GetToken.java
@@ -2,7 +2,7 @@ package com.where_car.emulator.device.domain.event;
 
 import com.where_car.emulator.device.domain.car.CarDevice;
 import com.where_car.emulator.device.domain.car.CarIdentity;
-import com.where_car.emulator.global.constants.DomainConstant;
+import com.where_car.emulator.global.constants.DeviceConstant;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -27,5 +27,5 @@ public class GetToken {
   private CarDevice carDevice;
 
   @Builder.Default
-  private String dFWVer = DomainConstant.FIRMWARE_VERSION;
+  private String dFWVer = DeviceConstant.FIRMWARE_VERSION;
 }
