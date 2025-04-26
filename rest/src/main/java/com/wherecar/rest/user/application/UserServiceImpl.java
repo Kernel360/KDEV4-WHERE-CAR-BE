724097f608768781eea5aafd07fae59dbaf58ac9
ijjung365 <ijjung365@gmail.com>
2025-04-26 15:12:15 +0900

    [#210] feat: 유저, 회사 로깅 추가

--- rest/src/main/java/com/wherecar/rest/user/application/UserServiceImpl.java
+++ rest/src/main/java/com/wherecar/rest/user/application/UserServiceImpl.java
@@ -39,6 +39,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserResponse createRoot(RootUserRequest rootUserRequest) {
+        log.info("[USER][UserServiceImpl][createRoot] 시작 | rootUserRequest={}", rootUserRequest);
 
         // 0. 필요한 파라미터 준비
         UserRequest userRequest = rootUserRequest.getUser();
@@ -59,11 +60,15 @@ public class UserServiceImpl implements UserService {
         user = userStore.store(user, Set.of(PermissionType.PERM_ADMIN));
 
         // 5. 유저 dto 반환
-        return userFactory.toUserResponse(user);
+        UserResponse userResponse = userFactory.toUserResponse(user);
+
+        log.info("[USER][UserServiceImpl][createRoot] 끝 | userResponse={}", userResponse);
+        return userResponse;
     }
 
     @Override
     public UserResponse createSub(SubUserRequest subUserRequest, Long companyId) {
+        log.info("[USER][UserServiceImpl][createSub] 시작 | subUserRequest={}, companyId={}", subUserRequest, companyId);
 
         // 0. 필요한 파라미터 준비
         UserRequest userRequest = subUserRequest.getUser();
@@ -83,43 +88,58 @@ public class UserServiceImpl implements UserService {
         user = userStore.store(user, permissionTypes);
 
         // 5. 유저 dto 반환
-        return userFactory.toUserResponse(user);
+        UserResponse userResponse = userFactory.toUserResponse(user);
+
+        log.info("[USER][UserServiceImpl][createSub] 끝 | userResponse={}", userResponse);
+
+        return userResponse;
     }
 
     @Override
     @Transactional(readOnly = true)
     public List<UserResponse> getUsersOfCompany(Long companyId) {
+        log.info("[USER][UserServiceImpl][getUsersOfCompany] 시작 | companyId={}", companyId);
         // 1. 유저조회
         List<User> users = userReader.getUsersByCompanyId(companyId);
 
         // 2. 유저 dto 리스트로 반환
-        return users.stream()
-                .map(userFactory::toUserResponse)
-                .collect(Collectors.toList());
+        List<UserResponse> userResponses = userFactory.toUserResponses(users);
+
+        log.info("[USER][UserServiceImpl][getUsersOfCompany] 끝 | userResponses={}", userResponses);
+        return userResponses;
     }
 
     @Override
     @Transactional(readOnly = true)
     public UserResponse getUserById(Long userId) {
+        log.info("[USER][UserServiceImpl][getUserById] 시작 | userId={}", userId);
         User user = userReader.getUserById(userId);
-        return userFactory.toUserResponse(user);
+        UserResponse userResponse = userFactory.toUserResponse(user);
+        log.info("[USER][UserServiceImpl][getUserById] 끝 | userResponses={}", userResponse);
+        return userResponse;
     }
 
     @Override
     public void deleteUserById(Long userId) {
+        log.info("[USER][UserServiceImpl][deleteUserById] 시작 | userId={}", userId);
         userStore.delete(userId);
+        log.info("[USER][UserServiceImpl][deleteUserById] 끝");
     }
 
     @Override
     public UserResponse updateUserById(Long userId, UserRequest userRequest) {
+        log.info("[USER][UserServiceImpl][updateUserById] 시작 | userId={}, userRequest={}", userId, userRequest);
         User user = userReader.getUserById(userId);
         user.updateUser(userRequest);
         userStore.store(user);
-        return userFactory.toUserResponse(user);
+        UserResponse userResponse = userFactory.toUserResponse(user);
+        log.info("[USER][UserServiceImpl][updateUserById] 끝 | userResponse={}", userResponse);
+        return userResponse;
     }
 
     @Override
     public UserResponse updatePasswordById(Long userId, PasswordRequest passwordRequest) {
+        log.info("[USER][UserServiceImpl][updatePasswordById] 시작 | userId={}, passwordRequest={}", userId, passwordRequest);
         User user = userReader.getUserById(userId);
 
         //비밀번호 확인 로직 실행
@@ -128,31 +148,38 @@ public class UserServiceImpl implements UserService {
         }
         // 유저 저장
         userStore.store(user);
-
-        return userFactory.toUserResponse(user);
+        UserResponse userResponse = userFactory.toUserResponse(user);
+        log.info("[USER][UserServiceImpl][updatePasswordById] 끝 | userResponse={}", userResponse);
+        return userResponse;
     }
 
     //권한
 
     @Override
     public UserResponse updatePermission(Long userId, PermissionRequest permissionRequest) {
+        log.info("[USER][UserServiceImpl][updatePermission] 시작 | userId={}, permissionRequest={}", userId, permissionRequest);
         User user = userReader.getUserById(userId);
         Set<PermissionType> permissionTypes = permissionRequest.getPermissionTypes();
 
         user = userStore.store(user,permissionTypes);
-        return userFactory.toUserResponse(user);
+        UserResponse userResponse = userFactory.toUserResponse(user);
+        log.info("[USER][UserServiceImpl][updatePermission] 끝 | userResponse={}", userResponse);
+        return userResponse;
     }
 
     @Override
     @Transactional(readOnly = true)
     public PermissionResponse getPermissionById(Long userId) {
+        log.info("[USER][UserServiceImpl][getPermissionById] 시작 | userId={}", userId);
         User user = userReader.getUserById(userId);
 
         Set<PermissionType> permissionTypes = user.getUserPermissions().stream()
                 .map(userPermission -> userPermission.getPermission().getType())
                 .collect(Collectors.toSet());
 
-        return new PermissionResponse(permissionTypes);
+        PermissionResponse permissionResponse  = new PermissionResponse(permissionTypes);
+        log.info("[USER][UserServiceImpl][getPermissionById] 끝 | permissionResponse={}", permissionResponse);
+        return permissionResponse;
     }
 
     private void checkEmailDuplication(String email){
