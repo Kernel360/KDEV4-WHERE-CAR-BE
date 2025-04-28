87aff4a4485f1a0f16e45ac38a1c2ef85179b393
codingjjh0515 <greenjjh0515@naver.com>
2025-04-28 16:36:36 +0900

    [#234] 이메일 검증 안 되는 버그 수정

--- rest/src/main/java/com/wherecar/rest/user/application/dto/SubUserRequest.java
+++ rest/src/main/java/com/wherecar/rest/user/application/dto/SubUserRequest.java
@@ -1,11 +1,15 @@
 package com.wherecar.rest.user.application.dto;
 
+import jakarta.validation.Valid;
 import lombok.*;
 
 @Setter
 @Getter
 @ToString
 public class SubUserRequest {
+
     PermissionRequest permission;
+
+    @Valid
     UserRequest user;
 }
