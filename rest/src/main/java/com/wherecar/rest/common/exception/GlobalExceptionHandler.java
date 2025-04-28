d42a6b7d6c61ec5205463a79441b1fc8a428ead6
codingjjh0515 <greenjjh0515@naver.com>
2025-04-28 12:09:59 +0900

    [#222] 검증 관련 @ExceptionHandler 추가

--- rest/src/main/java/com/wherecar/rest/common/exception/GlobalExceptionHandler.java
+++ rest/src/main/java/com/wherecar/rest/common/exception/GlobalExceptionHandler.java
@@ -1,15 +1,34 @@
 package com.wherecar.rest.common.exception;
 
 import com.wherecar.rest.common.response.BaseResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+import java.util.NoSuchElementException;
+
+@Slf4j
+@RestControllerAdvice(basePackages = "com.wherecar.rest")
 public class GlobalExceptionHandler {
+
+    // TODO 검증과 별개의 예외들도 처리하기
 
     @ExceptionHandler
     public ResponseEntity<BaseResponse<Void>> handleException(Exception e) {
+
         return BaseResponse.badRequest(e.getMessage());
     }
+
+    @ExceptionHandler
+    public ResponseEntity<BaseResponse<Void>> handleValidationExceptions(MethodArgumentNotValidException e) {
+        e.getBindingResult().getAllErrors().forEach(error -> {
+            String errorMessage = error.getDefaultMessage();
+            log.info("Validation 실패: {}", errorMessage);
+        });
+
+        return BaseResponse.badRequest(e.getMessage());
+    }
+
 }
