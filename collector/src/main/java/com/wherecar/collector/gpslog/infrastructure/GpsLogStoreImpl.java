7daea0808e5b9099af4da4e364d40a6a271659a5
codingjjh0515 <greenjjh0515@naver.com>
2025-05-04 17:12:07 +0900

    feat: 동기

--- collector/src/main/java/com/wherecar/collector/gpslog/infrastructure/GpsLogStoreImpl.java
+++ collector/src/main/java/com/wherecar/collector/gpslog/infrastructure/GpsLogStoreImpl.java
@@ -19,7 +19,6 @@ public class GpsLogStoreImpl implements GpsLogStore {
     private final CarStatusRepository carStatusRepository;
 
     @Override
-    @Transactional
     public void store(List<GpsLog> gpsLogList, Car car, List<String> batList) {
 
         for (int i = 0; i < gpsLogList.size(); i++) {
