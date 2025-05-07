1abae69cd783b2046ccfcc2dbadbe4a407a602d3
codingjjh0515 <greenjjh0515@naver.com>
2025-05-07 17:17:15 +0900

    feat: 동기 again

--- collector/src/main/java/com/wherecar/collector/common/config/AsyncConfig.java
+++ collector/src/main/java/com/wherecar/collector/common/config/AsyncConfig.java
@@ -1,48 +1,48 @@
-package com.wherecar.collector.common.config;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.AsyncConfigurer;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.lang.reflect.Method;
-import java.util.concurrent.Executor;
-
-@Slf4j
-@Configuration
-@EnableAsync
-public class AsyncConfig implements AsyncConfigurer {
-
-    @Bean(name = "taskExecutor")
-    public Executor asyncExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-
-        executor.setCorePoolSize(8);
-        executor.setMaxPoolSize(200);
-        executor.setQueueCapacity(1000);
-        executor.setKeepAliveSeconds(60);
-        executor.setAllowCoreThreadTimeOut(false);
-        executor.setWaitForTasksToCompleteOnShutdown(false);
-        executor.setAwaitTerminationSeconds(0);
-        executor.setRejectedExecutionHandler(new java.util.concurrent.ThreadPoolExecutor.AbortPolicy());
-        executor.setThreadNamePrefix("test");
-
-        executor.initialize();
-        return executor;
-    }
-
-    @Override
-    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
-        return new AsyncUncaughtExceptionHandler() {
-
-            @Override
-            public void handleUncaughtException(Throwable ex, Method method, Object... params) {
-                log.error("비동기 메서드에서 예외 발생: method={}, message={}", method.getName(), ex.getMessage(), ex);
-            }
-        };
-    }
-
-}
+//package com.wherecar.collector.common.config;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.scheduling.annotation.AsyncConfigurer;
+//import org.springframework.scheduling.annotation.EnableAsync;
+//import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+//
+//import java.lang.reflect.Method;
+//import java.util.concurrent.Executor;
+//
+//@Slf4j
+//@Configuration
+//@EnableAsync
+//public class AsyncConfig implements AsyncConfigurer {
+//
+//    @Bean(name = "taskExecutor")
+//    public Executor asyncExecutor() {
+//        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+//
+//        executor.setCorePoolSize(8);
+//        executor.setMaxPoolSize(200);
+//        executor.setQueueCapacity(1000);
+//        executor.setKeepAliveSeconds(60);
+//        executor.setAllowCoreThreadTimeOut(false);
+//        executor.setWaitForTasksToCompleteOnShutdown(false);
+//        executor.setAwaitTerminationSeconds(0);
+//        executor.setRejectedExecutionHandler(new java.util.concurrent.ThreadPoolExecutor.AbortPolicy());
+//        executor.setThreadNamePrefix("test");
+//
+//        executor.initialize();
+//        return executor;
+//    }
+//
+//    @Override
+//    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+//        return new AsyncUncaughtExceptionHandler() {
+//
+//            @Override
+//            public void handleUncaughtException(Throwable ex, Method method, Object... params) {
+//                log.error("비동기 메서드에서 예외 발생: method={}, message={}", method.getName(), ex.getMessage(), ex);
+//            }
+//        };
+//    }
+//
+//}
