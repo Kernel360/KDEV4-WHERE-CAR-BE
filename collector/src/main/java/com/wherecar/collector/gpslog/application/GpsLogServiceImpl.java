284030ced5671b010b4d6559529159710e099f7e
jaekyung <rkj0103@naver.com>
2025-05-09 18:35:59 +0900

    feat: hub 비동기 / collector 동기

--- collector/src/main/java/com/wherecar/collector/gpslog/application/GpsLogServiceImpl.java
+++ collector/src/main/java/com/wherecar/collector/gpslog/application/GpsLogServiceImpl.java
@@ -15,40 +15,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Slf4j
-@Service
-@RequiredArgsConstructor
-public class GpsLogServiceImpl implements GpsLogService {
-
-    private final CarReader carReader;
-    private final GpsLogStore gpsLogStore;
-    private final GpsLogFactory gpsLogFactory;
-
-    @Override
-    @Async
-    public void receiveGpsLogs(GpsLogRequest gpsLogRequest) {
-        log.info("[GPSLOG][GpsLogServiceImpl][receiveGpsLogs] 시작 | gpsLogRequest = {}", gpsLogRequest);
-
-        try {
-            Car car = carReader.getCarByMdn(gpsLogRequest.getMdn());
-            List<String> batList = gpsLogRequest.getCList().stream()
-                    .map(GpsLogInfo::getBat)
-                    .toList();
-            List<GpsLog> gpsLogList = gpsLogFactory.toGpsLogList(gpsLogRequest);
-            gpsLogStore.store(gpsLogList, car, batList);
-            log.info("[GPSLOG][GpsLogServiceImpl][receiveGpsLogs] 끝");
-        } catch (Exception e) {
-            log.error("collector GPS 로그 저장 비동기 처리 예외 발생", e);
-            log.info("[GPSLOG][GpsLogServiceImpl][receiveGpsLogs] 끝");
-        }
-    }
-}
-
-
 //@Slf4j
 //@Service
 //@RequiredArgsConstructor
-//@Transactional
 //public class GpsLogServiceImpl implements GpsLogService {
 //
 //    private final CarReader carReader;
@@ -56,15 +25,46 @@ public class GpsLogServiceImpl implements GpsLogService {
 //    private final GpsLogFactory gpsLogFactory;
 //
 //    @Override
+//    @Async
 //    public void receiveGpsLogs(GpsLogRequest gpsLogRequest) {
 //        log.info("[GPSLOG][GpsLogServiceImpl][receiveGpsLogs] 시작 | gpsLogRequest = {}", gpsLogRequest);
 //
-//        Car car = carReader.getCarByMdn(gpsLogRequest.getMdn());
-//        List<String> batList = gpsLogRequest.getCList().stream()
-//                .map(GpsLogInfo::getBat)
-//                .toList();
-//        List<GpsLog> gpsLogList = gpsLogFactory.toGpsLogList(gpsLogRequest);
-//        gpsLogStore.store(gpsLogList, car, batList);
-//        log.info("[GPSLOG][GpsLogServiceImpl][receiveGpsLogs] 끝");
+//        try {
+//            Car car = carReader.getCarByMdn(gpsLogRequest.getMdn());
+//            List<String> batList = gpsLogRequest.getCList().stream()
+//                    .map(GpsLogInfo::getBat)
+//                    .toList();
+//            List<GpsLog> gpsLogList = gpsLogFactory.toGpsLogList(gpsLogRequest);
+//            gpsLogStore.store(gpsLogList, car, batList);
+//            log.info("[GPSLOG][GpsLogServiceImpl][receiveGpsLogs] 끝");
+//        } catch (Exception e) {
+//            log.error("collector GPS 로그 저장 비동기 처리 예외 발생", e);
+//            log.info("[GPSLOG][GpsLogServiceImpl][receiveGpsLogs] 끝");
+//        }
 //    }
 //}
+
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class GpsLogServiceImpl implements GpsLogService {
+
+    private final CarReader carReader;
+    private final GpsLogStore gpsLogStore;
+    private final GpsLogFactory gpsLogFactory;
+
+    @Override
+    public void receiveGpsLogs(GpsLogRequest gpsLogRequest) {
+        log.info("[GPSLOG][GpsLogServiceImpl][receiveGpsLogs] 시작 | gpsLogRequest = {}", gpsLogRequest);
+
+        Car car = carReader.getCarByMdn(gpsLogRequest.getMdn());
+        List<String> batList = gpsLogRequest.getCList().stream()
+                .map(GpsLogInfo::getBat)
+                .toList();
+        List<GpsLog> gpsLogList = gpsLogFactory.toGpsLogList(gpsLogRequest);
+        gpsLogStore.store(gpsLogList, car, batList);
+        log.info("[GPSLOG][GpsLogServiceImpl][receiveGpsLogs] 끝");
+    }
+}
