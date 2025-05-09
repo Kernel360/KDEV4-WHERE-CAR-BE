dd5b9b3f5df32481d53e086057d7fa8c69a64b98
jaekyung <rkj0103@naver.com>
2025-05-09 14:31:11 +0900

    feat: hub 동기 / collector 동기

--- hub/src/main/java/com/wherecar/hub/gpslog/application/GpsLogHubService.java
+++ hub/src/main/java/com/wherecar/hub/gpslog/application/GpsLogHubService.java
@@ -4,5 +4,5 @@ import com.wherecar.hub.gpslog.application.dto.GpsLogRequest;
 import com.wherecar.hub.common.application.dto.MessageResponse;
 
 public interface GpsLogHubService {
-    void sendGpsLogMessage(GpsLogRequest gpsLogRequest);
+    MessageResponse sendGpsLogMessage(GpsLogRequest gpsLogRequest);
 }
