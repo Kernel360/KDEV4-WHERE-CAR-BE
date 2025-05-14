88038974301b47f7c11229d0e767dd9200e7278b
ijjung365 <ijjung365@gmail.com>
2025-05-14 22:13:20 +0900

    feat: QueueNameProvider 주석

--- collector/src/main/java/com/wherecar/collector/config/QueueNameProvider.java
+++ collector/src/main/java/com/wherecar/collector/config/QueueNameProvider.java
@@ -1,10 +1,10 @@
-package com.wherecar.collector.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-@Component("queueNameProvider")  // Bean 이름 명시
-public class QueueNameProvider {
-    @Value("${gps.queue-name}")
-    public String queueName;
-}
+//package com.wherecar.collector.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//@Component("queueNameProvider")  // Bean 이름 명시
+//public class QueueNameProvider {
+//    @Value("${gps.queue-name}")
+//    public String queueName;
+//}
